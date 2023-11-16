86c492e867335379907250f5ad06cd485a0dc29e
DevContinue <s_kapahnke@web.de>
2023-11-16 09:57:26 +0100

    add whitespace

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,5 +4,6 @@ import path from "path";
 const router = express.Router();
 
 export default router.get("^/$|/index(.html)?", (req: Request, res: Response) => {
+  
   res.sendFile(path.join(__dirname, "../../src", "views", "index.html"));
 });

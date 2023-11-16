98cf9eb0b3c76f513c2a1bc8694d6007233a76d6
DevContinue <s_kapahnke@web.de>
2023-11-16 13:21:07 +0100

    first change

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -4,6 +4,5 @@ import path from "path";
 const router = express.Router();
 
 export default router.get("^/$|/index(.html)?", (req: Request, res: Response) => {
-  
   res.sendFile(path.join(__dirname, "../../src", "views", "index.html"));
 });
